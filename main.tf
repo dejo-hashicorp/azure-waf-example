c30f41bd7be2012e051ce8e47d4dfecfa0164c78
Dejo Oyelese <dejo.oyelese@hashicorp.com>
2024-12-17 08:34:50 -0700

    update main, remove rule

--- main.tf
+++ main.tf
@@ -14,21 +14,6 @@ resource "azurerm_web_application_firewall_policy" "example" {
   }
 
   custom_rules {
-    name      = "AllowRule2"
-    priority  = 2
-    action    = "Allow"
-    rule_type = "MatchRule"
-
-    match_conditions {
-      match_variables {
-        variable_name = "RemoteAddr"
-      }
-      operator = "IPMatch"
-      match_values = ["10.0.0.0/8", "192.168.0.0/16"]
-    }
-  }
-
-  custom_rules {
     name      = "AllowRule1"
     priority  = 1
     action    = "Allow"
